65c61317b5088d62de1feb30bc33ded6a79b7775
yzinnie <yyjh222@gmail.com>
2024-02-08 15:05:15 +0900

    [🛠] Update Info
    --add link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
 
 const Header = () => {
@@ -23,7 +23,11 @@ const Header = () => {
       />
       <div className="menu">
         <ul className={isOpen ? 'navLinks showNav' : 'hidden'}>
-          <li onClick={() => navigate('/info')}>산 check?</li>
+          <li>
+            <Link to="https://yyjh222.notion.site/Check-5ff856bc067a4985ad29dbfeffb436df?pvs=4">
+              산-check?
+            </Link>
+          </li>
           <li onClick={() => navigate('/checkList')}>체크리스트</li>
           <li onClick={() => navigate('/news')}>반려동물을 검색했어요</li>
         </ul>
@@ -33,5 +37,3 @@ const Header = () => {
 };
 
 export default Header;
-
-//<img src={process.env.PUBLIC_URL + `assets/icon-menu.png`} />
